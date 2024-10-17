afb44d56c85ec457568832e5749c14803b6ca84d
Yacin Nadji <ynadji@gmail.com>
2024-10-17 07:43:17 -0400

    remove unneeded (in-package :cl-user) call

--- packages.lisp
+++ packages.lisp
@@ -1,4 +1,3 @@
-(in-package :cl-user)
 (defpackage :netaddr
   (:use :cl)
   (:local-nicknames (:ax :alexandria))
--- tests.lisp
+++ tests.lisp
@@ -1,4 +1,3 @@
-(in-package :cl-user)
 (defpackage netaddr/tests
   (:use #:cl #:netaddr)
   (:import-from #:fiveam
